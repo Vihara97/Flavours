2e4c8946be5904aeb67b374f14328c9c1e0e366a
Vihara Karunarathna <vihara97@gmail.com>
2023-01-15 03:00:57 -0800

    initial commit

---
+++ Flavours/FlavoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavoursApp.swift
+//  Flavours
+//
+//  Created by Vihara Karunarathna on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
