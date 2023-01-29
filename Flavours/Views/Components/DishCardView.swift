b9d9ecfa8d7d45be2f22849b4f0021e5181081c6
Vihara Karunarathna <vihara97@gmail.com>
2023-01-29 10:30:33 -0800

    URLImage package configuration

--- Flavours/Views/Components/DishCardView.swift
+++ Flavours/Views/Components/DishCardView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct DishCardView: View {
     
@@ -14,7 +15,17 @@ struct DishCardView: View {
     
     var body: some View {
         
+        let imgurl: URL = URL(string: "https://www.seriouseats.com/thmb/ydRx2uJ9KWLplPH6FOnPuvta1gw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__20120525-grilled-side-dishes-appetizers-memorial-day-09-07de349927bb4dd187546f9b5b7eec07.jpeg")!
+            
         NavigationView {
+            
+            URLImage(imgurl) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            
             if(dishModel.list.count > 0){
                 Text(dishModel.list[0].name)
             }else{
